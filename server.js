66ea07274ff0ad4c4c2aa774415d10c7870f069e
r2ndy <88577982+r2ndy@users.noreply.github.com>
2021-08-11 03:12:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -137,11 +137,10 @@ client.on('message', message => {
     if (!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply(`**هذه الخاصية للادارة فقط** :negative_squared_cross_mark: `)
     if (!message.channel.guild) return message.reply(' ');
     const millis = new Date().getTime() - message.guild.createdAt.getTime();
-    const now = new Date();
-    dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+    const now = [now, 'dddd, mmmm dS, yyyy, h:MM:ss TT'];
     const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
     const days = millis / 1000 / 60 / 60 / 24;
-    let roles = niro.guilds.get(message.guild.id).roles.map(r => r.name);
+    let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
     var embed = new Discord.RichEmbed()
       .setAuthor(message.guild.name, message.guild.iconURL)
       .addField("**🆔 Server ID:**", message.guild.id, true)
