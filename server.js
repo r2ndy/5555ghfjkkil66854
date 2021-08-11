93a494d65e7e41beee25ed9aabe37024f6cd7197
r2ndy <88577982+r2ndy@users.noreply.github.com>
2021-08-11 03:17:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -290,7 +290,78 @@ client.on('message', message => {
 
 //////////////////////////////////////////////////
 ////////// فك الميووت /////////// 
+ clien.on("message", message => {
+  if (message.author.bot) return;
 
+  let command = message.content.split(" ")[0];
+
+  if (command === prefix + "unmute") {
+    if (message.author.bot) return;
+    if (!message.member.hasPermission("MANAGE_ROLES"))
+      return message
+        .reply("** لا يوجد لديك برمشن 'Manage Roles' **")
+        .catch(console.error);
+    let user = message.mentions.users.first();
+    let modlog = niro.channels.find(gg => gg.name === "log");
+    let muteRole = niro.guilds
+      .get(message.guild.id)
+      .roles.find(gg => gg.name === "Muted");
+    if (!muteRole)
+      return message
+        .reply("** لا يوجد لديك رتبه الميوت 'Muted' **")
+        .catch(console.error);
+    if (message.mentions.users.size < 1)
+      return message
+        .channel.send(new Discord.RichEmbed().setDescription(`
+        **Command: unmute**
+
+Unmutes a member.
+
+**Usage:**
+#unmute (user)
+
+**Examples:**
+#unmute @ニロ
+#unmute 157605500488384512`))
+        .catch(console.error);
+    const embed = new Discord.RichEmbed()
+      .setColor(0x00ae86)
+      .setTimestamp()
+      .addField("الأستعمال:", "اسكت/احكي")
+      .addField(
+        "تم فك الميوت عن:",
+        `${user.username}#${user.discriminator} (${user.id})`
+      )
+      .addField(
+        "بواسطة:",
+        `${message.author.username}#${message.author.discriminator}`
+      );
+
+    if (
+      !message.guild
+        .member(niro.user)
+        .hasPermission("MANAGE_ROLES_OR_PERMISSIONS")
+    )
+      return message
+        .reply("** لا يوجد لدي برمشن Manage Roles **")
+        .catch(console.error);
+
+    if (message.guild.member(user).removeRole(muteRole.id)) {
+      return message
+        .reply("**:white_check_mark: .. تم فك الميوت عن الشخص **")
+        .catch(console.error);
+    } else {
+      message.guild
+        .member(user)
+        .removeRole(muteRole)
+        .then(() => {
+          return message
+            .reply("**:white_check_mark: .. تم فك الميوت عن الشخص **")
+            .catch(console.error);
+        });
+    }
+  }
+});
 //////////////////////
 ///////////// باند وفك الباند /////////
 
