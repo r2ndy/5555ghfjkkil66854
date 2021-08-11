336f5b6599a2293f8339528fcb9de4a7eb57f347
r2ndy <88577982+r2ndy@users.noreply.github.com>
2021-08-11 20:42:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -129,7 +129,23 @@ client.on('message', msg =>{
 
 /////////////////////////////////////////////////////
 /////// معلومات السيرفر ///// 
-
+client.on("message", message =>{
+    var guild = message.guild;
+if(message.guild.channel) return;
+if(message.content.startsWith(prefix + "server")){
+    let embed = new Discord.RichEmbed()
+    .setTitle(`${guild}`)
+    .addField("🆔 ID Server", `${message.guild.id}`)
+    .addField("📆 Created At",`${moment(guild.createdTimestamp).format('D/M/YYYY H:ss')} \n${moment(guild.createdTimestamp).fromNow()}`)
+    .addField(":crown: Owned By", `${guild.owner}`)
+    .addField(`👥** Members ${guild.memberCount}**`,`**${message.guild.members.filter
+(m=>m.presence.status === "online").size}** Online`)
+.addField(`:speech_balloon: Channels **(${guild.channels.size})**`, `**${guild.channels.filter(c =>c.type === "text").size}** Text | **${guild.channels.filter(c=>c.type === "voice").size}** Voice |** ${guild.channels.filter(c=>c.type ==="category").size}** Cateogory`)
+.addField(`**:earth_africa: Others **`,`**Region : ** ${guild.region} \n ** Verification Level : **${guild.verificationLevel}`)
+.addField(":closed_lock_with_key: Roles ",`**(${guild.roles.size})**`)
+    message.channel.send(embed);
+}
+})
 
 ///////////////////////////////////
 ///////// فقل و فتح الشات ////////////
@@ -276,82 +292,7 @@ client.on('guildMemberAdd', member => {
 
 //////////////////////////////////////////////////
 /////// كود الايدي ////////////// 
- client.on('message' , message => {
-    var prefix = '-';
-     const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
- if(message.content.split(' ')[0].toLowerCase() == prefix + 'id') {
-     const millis = new Date().getTime() - message.guild.createdAt.getTime();
-    const now = new Date();
-    const createdAt = millis / 1000 / 60 / 60 / 24;
-    if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
-    let user = message.mentions.users.first() || message.author;
-    message.delete();
-   
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
 
-    let game;
-    if (user.presence.game === null) {
-        game = 'None.';
-    } else {
-        game = user.presence.game.name;
-    }
-    let messag;
-    if (user.lastMessage === null) {
-        messag = 'None.';
-    } else {
-        messag = user.lastMessage;
-    }
-    let status;
-    if (user.presence.status === 'online') {
-        status = 'Online';
-    } else if (user.presence.status === 'dnd') {
-        status = 'DND';
-    } else if (user.presence.status === 'idle') {
-        status = 'Idle';
-    } else if (user.presence.status === 'offline') {
-        status = 'Offline';
-    }
-    if (user.presence.status === 'offline') {
-        stat = 0x000000;
-    } else if (user.presence.status === 'online') {
-        stat = 0x00AA4C;
-    } else if (user.presence.status === 'dnd') {
-        stat = 0x9C0000;
-    } else if (user.presence.status === 'idle') {
-        stat = 0xF7C035;
-    }
-    moment.locale('En-ly');
-                    message.guild.fetchInvites().then(invs => {
-      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-      let Invites = invs.filter(i => i.inviter.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-    const embed = new Discord.RichEmbed()
-
-  
-  .addField('Discord Info : ', `Name : ${user.username}\n Discriminator: #${user.discriminator}\nID : ${user.id} \nJoinedDiscord : ${moment(heg.createdTimestamp).fromNow()}\nBot :  ${user.bot}\nPlaying : ${game}\nStatus : ${status}`,true)
-  .addField('Server Info :', `LastMessage : ${messag}\nJoined :  ${moment(h.joinedAt).fromNow()} \n Invites :  ${inviteCount} Invite(s) \nRoles : `+message.guild.members.get(user.id).roles.array(role => role.name).slice(1).join(', '))
-  .setAuthor(`${user.username}`, user.displayAvatarURL)
-  .setColor('#36393e')
-    .setThumbnail(user.displayAvatarURL)
-    message.channel.send({embed})
-  .catch(e => logger.error(e));
- })
-}
- });
 
 /////////////////
 client.login(process.env.BOT_TOKEN);
