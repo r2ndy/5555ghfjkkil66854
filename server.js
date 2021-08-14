80daaa826a645969590b3ea021f04fed823588e8
r2ndy <88577982+r2ndy@users.noreply.github.com>
2021-08-14 03:30:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -293,7 +293,7 @@ client.on("message", hosam => {
 
 client.on("message", message => {
     if (message.content === (prefix + "help")) {
-    const embed = new Discor.MessageEmbed()  
+    const embed = Discor.MessageEmbed()  
          .setColor("#580e6b")
          .setThumbnail(message.author.avatarURL)
          .setDescription(`
